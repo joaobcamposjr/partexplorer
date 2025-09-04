7b1069c2317bf22df08ef490e1f901aa0896a38c
João Campos - Mac <joaobcamposjr@gmail.com>
2025-09-03 22:17:59 -0300

    Test: Mudança simples para testar ambientes independentes
    - Alterado título para 'Catalogo v2.0'
    - Teste para verificar se DEV e PROD são independentes
    - Verificar se Actions funciona corretamente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -401,7 +401,7 @@ function App() {
                 </svg>
               </div>
               <h1 className="text-2xl font-bold text-gray-800">
-                Catalogo
+                Catalogo v2.0
               </h1>
             </div>
 
